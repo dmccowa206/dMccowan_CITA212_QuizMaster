5da7dc60d5c5f56ca6da0bd89be5ef50ca566e57
dmccowa206 <mccowa206@morrisville.edu>
2024-10-09 13:21:23 -0400

    Used for loop to populate answer buttons with options.

--- Assets/Scripts/Quiz_scr.cs
+++ Assets/Scripts/Quiz_scr.cs
@@ -7,9 +7,15 @@ public class Quiz_scr : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField]GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 }
