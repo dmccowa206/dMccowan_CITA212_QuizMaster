50657f2eeea2e1571df8958720f68a54d097be97
dmccowa206 <mccowa206@morrisville.edu>
2024-10-16 13:50:13 -0400

    Created DisplayQuestion method and handled button states.
    Buttons are turned on when a new question is displayed and turned off when an answer is chosen.

--- Assets/Scripts/Quiz_scr.cs
+++ Assets/Scripts/Quiz_scr.cs
@@ -15,12 +15,7 @@ public class Quiz_scr : MonoBehaviour
 
     void Start()
     {
-        questionText.text = question.GetQuestion();
-        for(int i = 0; i < answerButtons.Length; i++)
-        {
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
-        }
+        GetNextQuestion();
     }
     public void OnAnswerSelected(int index)
     {
@@ -38,6 +33,38 @@ public class Quiz_scr : MonoBehaviour
             questionText.text = "Sorry, the correct answer was\n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+        }
+        SetButtonState(false);
+    }
+    private void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+    private void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+    private void SetButtonState(bool state)
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+    private void SetDefaultButtonSprites()
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
         }
     }
 }
