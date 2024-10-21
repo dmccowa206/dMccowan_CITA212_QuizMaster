4a18211cefc3e6e00795c007cc4a56e899c50d77
dmccowa206 <mccowa206@morrisville.edu>
2024-10-21 13:37:54 -0400

    Added end screen to display final score.

---
+++ Assets/Scripts/EndScreen_scr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScreen_scr : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper_scr scoreKeep;
+    void Start()
+    {
+        scoreKeep = FindAnyObjectByType<ScoreKeeper_scr>();        
+    }
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou got a score of " +
+                                scoreKeep.CalculateScore();
+    }
+}
